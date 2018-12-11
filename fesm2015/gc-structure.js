abf00bcdcd15a3390078690a6e5952bbf145a699
Samuel Barney <samjbarney@gmail.com>
2018-12-11 00:53:51 -0700

    Updated to allow for develop.gamechanger.studio urls

--- fesm2015/gc-structure.js
+++ fesm2015/gc-structure.js
@@ -55,12 +55,28 @@ class GCFooterComponent {
         // If the target location is another app, build an external url to pass back
         if (this.location !== target_location) {
             data.type = 'external';
+            /** @type {?} */
+            let url = window.location.href
             // Build a different url depending on where app needs to go
-            switch (target_location) {
-                case 'bbl':
-                    data.path = `https://bbl.gamechanger.studio/#/%{teamId}/${path}`;
-                case 'team-site':
-                    data.path = `https://${this.teamId}.gamechanger.studio/#/`;
+            // This block of code is horrible
+            ;
+            // Build a different url depending on where app needs to go
+            // This block of code is horrible
+            if (!url.startsWith('https://develop.gamechanger.studio')) {
+                switch (target_location) {
+                    case 'bbl':
+                        data.path = `https://bbl.gamechanger.studio/#/${path}`;
+                    case 'team-site':
+                        data.path = `https://${this.teamId}.gamechanger.studio/#/`;
+                }
+            }
+            else {
+                switch (target_location) {
+                    case 'bbl':
+                        data.path = `https://develop.gamechanger.studio/big-bash-buddy/#/${path}`;
+                    case 'team-site':
+                        data.path = `https://develop.gamechanger.studio/${this.teamId}/#/`;
+                }
             }
         }
         // Send the path info off to any registered handler
